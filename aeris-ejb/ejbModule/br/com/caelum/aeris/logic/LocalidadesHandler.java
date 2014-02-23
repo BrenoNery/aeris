d8402dd9b5a6adf5ede377a5dcdf201e4bd0729a
Breno Nery <brenosilvanery@gmail.com>
2014-02-23 15:22:18 -0300

    Implementação do padrão 'Factory', anotação Seam '@Factory'.

--- aeris-ejb/ejbModule/br/com/caelum/aeris/logic/LocalidadesHandler.java
+++ aeris-ejb/ejbModule/br/com/caelum/aeris/logic/LocalidadesHandler.java
@@ -2,7 +2,7 @@ package br.com.caelum.aeris.logic;
 
 import java.util.*; // Deste import vem os tipos: "Map" e "HashMap" corretos, onde um é compatível com o outro.
 
-//import org.jboss.seam.annotations.Factory; // Import da anotação "@Factory" (Utilizando o padrão Factory)
+import org.jboss.seam.annotations.Factory; // Import da anotação "@Factory" (Utilizando o padrão Factory)
 import org.jboss.seam.annotations.Name; // Import da anotação "@Name" (Criando um ManagedBean)
 
 import br.com.caelum.aeris.entity.Localidade;
@@ -10,7 +10,7 @@ import br.com.caelum.aeris.entity.Localidade;
 @Name("localidadesHandler")
 public class LocalidadesHandler {
 	
-	//@Factory("localidades")
+	@Factory("localidades")
 	public Map<String, Localidade> getLocalidades() {
 		System.out.println("Iniciando localidades");
 		
