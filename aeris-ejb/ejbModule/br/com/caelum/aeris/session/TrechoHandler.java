e043f4813e29c9128a643385e148e85dee3a8935
Breno Nery <brenosilvanery@gmail.com>
2014-02-25 09:23:06 -0300

    Inclusão do DataTable que exibe os Trechos.

--- aeris-ejb/ejbModule/br/com/caelum/aeris/session/TrechoHandler.java
+++ aeris-ejb/ejbModule/br/com/caelum/aeris/session/TrechoHandler.java
@@ -1,13 +1,17 @@
 package br.com.caelum.aeris.session;
 
+import java.util.*;
+
 import javax.persistence.EntityManager;
 
 import br.com.caelum.aeris.entity.Trecho;
 
 import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Factory; // Import da anotação "@Factory" (Padrão Fábrica)
 import org.jboss.seam.annotations.In; // Import da anotação "@In" (Injeção de dependencia)
 import org.jboss.seam.annotations.Name; // Import da anotação "@Name" (Criando um ManagedBean)
 import org.jboss.seam.annotations.Scope; // Import da anotação "@Scope" (Criando um ManagedBean)
+import org.jboss.seam.annotations.datamodel.DataModel; // Import da anotação "@DataModel" (Converte um List<> de beans para um DataModel)
 
 @Name("trechoHandler")
 @Scope(ScopeType.EVENT)
@@ -18,6 +22,9 @@ public class TrechoHandler {
 	@In
 	private EntityManager entityManager;
 	
+	@DataModel
+	private List<Trecho> trechos;
+	
 	public void salvar() {
 		
 		System.out.println("Salvando: " + trecho);
@@ -27,5 +34,12 @@ public class TrechoHandler {
 	
 	public Trecho getTrecho() {
 		return this.trecho;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Factory("trechos")
+	public void populaTrechos() {
+		System.out.println("Buscando trechos do Banco de Dados");
+		this.trechos = this.entityManager.createQuery("from Trecho").getResultList();
 	}
 }
